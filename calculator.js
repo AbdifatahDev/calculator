8ef941d99e2fbe4d4a2fd145431b3f2c2862561e
sharifka <Abdifatahsharif790@gmail.com>
2023-07-08 14:28:21 +0300

    added calculate by type

--- calculator.js
+++ calculator.js
@@ -13,21 +13,34 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    var num1= Number(req.body.n1);
-    var num2= Number(req.body.n2);
-    var result=num1+num2;
-  res.send("the result is "+result);
+  var type = req.body.type;
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = 0;
+  if (type == "add") {
+    result = num1 + num2;
+    res.send("the result is " + result);
+  } else if (type == "sub") {
+    result = num1 - num2;
+    res.send("the result is " + result);
+  } else if (type == "mul") {
+    result = num1 * num2;
+    res.send("the result is " + result);
+  } else if (type == "div") {
+    result = num1 / num2;
+    res.send("the result is " + result);
+  }
 });
 
-app.get("/bmicalculator",function(req,res){
-    res.sendFile(__dirname +"/bmiCalculator.html");
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/bmicalculator",function(req,res){
-    var weight= parseFloat(req.body.w);
-    var height= parseFloat(req.body.h);
-    var bmiResult= weight/Math.pow(height, 2);
-    res.send("Bmi Result is "+bmiResult);
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.w);
+  var height = parseFloat(req.body.h);
+  var bmiResult = weight / Math.pow(height, 2);
+  res.send("Bmi Result is " + bmiResult);
 });
 
 app.listen(port, function () {
